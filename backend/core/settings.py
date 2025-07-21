a93eb2e6eeea6db4dec4eb50ccb70860edd8e804
Harshita Pathak <harshita@veersatech.com>
2025-07-21 14:13:11 +0530

    deploy setting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
